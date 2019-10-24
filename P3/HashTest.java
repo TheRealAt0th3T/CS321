243176913e346aea934333ae201f62b6b00ee01c
TheRealAt0th3T <47465309+TheRealAt0th3T@users.noreply.github.com>
2019-10-24 14:56:45 -0600

    Create HashTest.java

---
+++ P3/HashTest.java
@@ -0,0 +1,22 @@
+/*
+hashtable --  creates array of hashobjs cna have counts here
+hashtest - creates the hashtable amd has the probs and the counts 
+end of program should be able to print all the numbers
+*/
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Random;
+import java.util.Scanner;
+
+/*
+ * 
+ * AUTHOR: AMARA TARIQ
+ * CS321 
+ * PROJECT 3 - HASHTABLES
+ * 
+ * HashTest.java -- driver class
+ * 
+ */
+
+
