07d224ffae80d03d999be42ad25ac484b43cbc36
TheRealAt0th3T <47465309+TheRealAt0th3T@users.noreply.github.com>
2019-10-24 18:41:06 -0600

    Update HashTable.java

--- P3/HashTable.java
+++ P3/HashTable.java
@@ -15,10 +15,14 @@ public class HashTable <T>{
 	public int size; //size of table
 	public int debugLevel; 
 	
+	private int linearProbeCount, doubleProbeCount;
+	private int linearInput, doubleInput;
+	private int duplicateLinearCount, duplicateDoubleCount;
+	
 	public HashTable(int load, int debug) {
 		load = this.loadFactor;
 		debug = this.debugLevel;
-		//size = ;
+		//size = ; // --------------based off the primeGeneraator
 		Table = new HashObject[size];
 	}
 	
@@ -39,6 +43,47 @@ public class HashTable <T>{
 	
 	public int doubleHashFunction(int key) {
 		return 1 + (key % (this.size-2));
+	}
+	
+	/*
+	 * PROBING AND HASHING METHODS
+	 */
+	
+	public int linearProbe(HashObject<T> obj) {
+		int itr = 0; //iterations
+		int objKey = obj.getKey();
+		int index = (linearHashFunction(key) + itr) % this.size;
+		linearProbeCount++; 
+		while(i<this.size) {
+			itr++;
+			if(Table[index] != null && Table[index]==obj) { //COLLISION
+				
+				duplicateLinearCount++; //because found obj -> collision
+				return index; 
+			} else if( table[index] == null) { //INSERTS OBJ
+				Table[index] = obj;
+				return index;
+			}
+		}
+	}
+	
+	public void doubleHashing(HashObject<T> obj) {
+		int objKey = obj.getKey();
+		int index = key % this.size;
+		int step = doubleHashFunction(objKey);
+		
+		while(Table[index]!= null) {
+			if(Table[index] == obj) { //found the obj already in that place
+				
+				return; //breaks out once finds a duplicate
+			}else { //space filled so need to take a step
+				index = index + step;
+				index = index % size;
+			}
+		}
+		if(Table[index] == null) {
+			table[index] = obj;
+		}
 	}
 	
 	
