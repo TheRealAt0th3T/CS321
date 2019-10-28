92cbb5111d1afeb945da38d70e0d3b9003198ef6
TheRealAt0th3T <47465309+TheRealAt0th3T@users.noreply.github.com>
2019-10-28 17:45:57 -0600

    stillToFinish

--- P3/HashTest.java
+++ P3/HashTest.java
@@ -19,25 +19,59 @@ import java.util.Scanner;
  * 
  */
 
-public class HashTest{
+public class HashTest {
 	private static double loadFactor;
-	public static void main (String[] args) {
-		loadFactor = Double.parseDouble(args[1]); //grabbing load factor from input
-		
-		if(args.length == 2 || Integer.parseInt(args[2]) == 0) {
-			if(Integer.parseInt(args[0]) ==1) {
-				//debug smth
-				//exit?
-			}else if(Integer.parseInt(args[0]==2)) {
-				//debug smth
-				//exit
+
+	public static void main(String[] args) {
+		loadFactor = Double.parseDouble(args[1]); // grabbing load factor from input
+
+		if (args.length == 2 || Integer.parseInt(args[2]) == 0) {
+			if (Integer.parseInt(args[0]) == 1) { // INPUT TYPE 1
+				// wat
+				// exit?
+			} else if (Integer.parseInt(args[0]) == 2) { //INPUT TYPE 2
+				// debug smth
+				// exit
+			}else if(Integer.parseInt(args[0]) ==3){ //INPUT TYPE 3
+				//
+				//
+			}
+		}
+		if (args.length == 3) { // IF THREE ARGUMENTS PASSED IN - INPUT. LOAD AND DEBUG
+			int debugLevel = Integer.parseInt(args[2]);
+			if (Integer.parseInt(args[0]) == 1) { //INPUT TYPE 1
+				if (debugLevel == 1) { //DEBUG LEVEL 1
+
+				}
+				if (debugLevel == 2) { //DEBUG LEVEL 2
+
+				}
+			} else if (Integer.parseInt(args[0]) == 2) { //INPUT TYPE 2
+				if (debugLevel == 1) { //DEBUG LEVEL 1
+
+				}
+				if (debugLevel == 2) { //DEBUG LEVEL 2
+
+				}
+			} else if (Integer.parseInt(args[0]) == 3) {//INPUT TYPE 3
+				if (debugLevel == 1) { //DEBUG LEVEL 1
+
+				}
+				if (debugLevel == 2) { //DEBUG LEVEL 2
+
+				}
 			}
 		}
 		
-		//if(args.length =)
-		
+		public static void debugLevelZero() {
+			HashTable<Integer> Linear = new HashTable<Integer>(load, 0);
+			DoubleHash<Integer> Double = new DoubleHash<Integer>(load, 0);
+		}
+
+		// if(args.length =)
+
 //		PrimeGenerator p = new PrimeGenerator();
 //		System.out.println(p.TwinPrimes(95500, 96000));
 	}
-	
+
 }
